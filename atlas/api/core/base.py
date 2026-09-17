9799c5e0ee9025542714ae928d964586a32ce9f5
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-09-17 23:06:23 +0530

    feat(api): Report pending host capacity

--- atlas/api/core/base.py
+++ atlas/api/core/base.py
@@ -374,6 +374,9 @@ class RouteHandler:
 		for name, value in self.router.default_headers.items():
 			response.headers[name] = value
 
+		if body["error"]["code"] == "capacity_pending":
+			response.headers["Retry-After"] = "15"
+
 		return response
 
 
