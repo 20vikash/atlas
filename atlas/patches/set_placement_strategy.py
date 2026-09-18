5e734db4b461722ed7c3b2c62535ffad289a76f8
ayushschaudhari1904@gmail.com <ayushschaudhari1904@gmail.com>
2026-09-18 13:44:36 +0530

    chore(placement): Remove migration patches

--- atlas/patches/set_placement_strategy.py
+++
@@ -1,6 +0,0 @@
-import frappe
-
-
-def execute() -> None:
-	if frappe.db.get_singles_dict("Atlas Settings").get("placement_strategy") is None:
-		frappe.db.set_single_value("Atlas Settings", "placement_strategy", "Default")
--- atlas/patches/set_sleepy_vm_overcommit_factor.py
+++
@@ -1,6 +0,0 @@
-import frappe
-
-
-def execute() -> None:
-	if frappe.db.get_singles_dict("Atlas Settings").get("sleepy_vm_overcommit_factor") is None:
-		frappe.db.set_single_value("Atlas Settings", "sleepy_vm_overcommit_factor", 1.0)
